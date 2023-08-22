77025a6d332de0363941b6524fa10631615c4e13
Adrian Nastał <adriannastal9@gmail.com>
2023-08-22 19:02:18 +0200

    fix2

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -38,7 +38,7 @@ const ContactForm = ({ contacts, onAddContact }) => {
         <input
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я\\s'-]+$"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces."
           required
           value={name}
@@ -49,7 +49,7 @@ const ContactForm = ({ contacts, onAddContact }) => {
         <input
           type="tel"
           name="number"
-          pattern="\\+?\\d{1,4}[-.\\s]?\\(?\d{1,3}?\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           value={number}
